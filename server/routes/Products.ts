0e4daab2b5832e913332230b9c953c576e11f0a7
apardor <apardor@gmail.com>
2023-02-09 12:55:28 +0100

    removed internal .git

---
+++ server/routes/Products.ts
@@ -0,0 +1,10 @@
+import express, {Router} from 'express';
+
+const router = Router();
+
+
+router.get('/product', () => {})
+router.get('/product/:id', () => {})
+router.patch('/product/:id', () => {})
+router.post('/product', () => {})
+router.delete('/product/:id', () => {})
---
+++ server/routes/User.ts
@@ -0,0 +1,10 @@
+import express, {Router} from 'express';
+
+const router = Router();
+
+
+router.get('/user', () => {})
+router.get('/user/:id', () => {})
+router.patch('/user/:id', () => {})
+router.post('/user', () => {})
+router.delete('/user/:id', () => {})
